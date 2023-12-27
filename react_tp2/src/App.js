898d4f0eba1aecd42e6a677e23d334eb82ff0809
valeriemo <v_moreau@hotmail.com>
2023-12-27 13:37:04 -0500

    initial commit creation projet react

---
+++ react_tp2/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+    return (
+        <div>
+            <div className="bg-blue-800 text-white m-4">
+                This is a component with Tailwind CSS styles.
+            </div>
+        </div>
+    );
+}
+
+export default App;
