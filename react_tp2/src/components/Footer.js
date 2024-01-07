3a3f376f92c03d1c9fd1a7b462aa45f49a32d150
valeriemo <v_moreau@hotmail.com>
2024-01-07 12:03:46 -0500

    retouche design

--- react_tp2/src/components/Footer.js
+++ react_tp2/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 p-6 mt-[150px] text-center text-sm">
+        <footer className="bg-gray-800 p-6 mt-[150px] text-center text-gray-300 text-sm">
             <h3 className="text-[#6290C3] pb-3 font-bold">Tp 2 | Valérie Moreau</h3>
             <p>Techniques avancées en programmation Web 582-41F-MA</p>
             <p className="text-[#6290C3]">Collège de Maisonneuve | 2024</p>
--- react_tp2/src/components/Nav.js
+++ react_tp2/src/components/Nav.js
@@ -6,9 +6,9 @@ const Nav = () => {
         <nav className="bg-gray-900 p-6">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to="/" className="text-[#F2F3D9] text-lg font-semibold">
-                    <Logo />
+                    <Logo color={"#C2E7DA"}/>
                 </Link>
-                <Link to="/films" className="text-[#F2F3D9] text-xl font-semibold hover:bg-[#e9295c] p-2">
+                <Link to="/films" className="btn-3">
                     Collection
                 </Link>
             </div>
